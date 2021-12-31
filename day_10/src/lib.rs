e7f714b022a741edcbc4fbb0dd89d7d02a985174
guidota <guido@decentraland.org>
2021-12-31 16:25:32 -0300

    feat: day 10

--- day_10/src/lib.rs
+++ day_10/src/lib.rs
@@ -4,24 +4,32 @@ mod tests {
 
     #[test]
     fn part1_example() {
-        part1("day_10/resources/example.txt");
+        let result = part1("day_10/resources/example.txt");
+        assert_eq!(result, 26397);
     }
 
     #[test]
     fn part1_input() {
-        part1("day_10/resources/input.txt");
+        let result = part1("day_10/resources/input.txt");
+        assert_eq!(result, 323691);
     }
 
     #[test]
-    fn part2_example() {}
+    fn part2_example() {
+        let result = part2("day_10/resources/example.txt");
+        assert_eq!(result, 288957)
+    }
 
     #[test]
-    fn part2_input() {}
+    fn part2_input() {
+        let result = part2("day_10/resources/input.txt");
+        assert_eq!(result, 2858785164)
+    }
 }
 
 use utils::*;
 
-pub fn part1(path: &str) {
+pub fn part1(path: &str) -> i32 {
     let input = Input::new(path);
     let mut total_syntax_error_score = 0;
     for line in input {
@@ -41,40 +49,77 @@ pub fn part1(path: &str) {
         }
     }
     println!("Total syntax error score: {}", total_syntax_error_score);
+    total_syntax_error_score
 }
 
-fn test_line(line: String) -> Result<(), (char, char)> {
-    let mut stack = vec![];
+pub fn part2(path: &str) -> usize {
+    let input = Input::new(path);
+    let mut scores = vec![];
+
+    for line in input {
+        match test_line(line) {
+            Ok(open_chars) => {
+                if open_chars.len() > 0 {
+                    let score = open_chars
+                        .iter()
+                        .rev()
+                        .fold(0, |acc, char| acc * 5 + get_close_char_score(char));
+
+                    scores.push(score);
+                }
+            }
+            Err(_) => (),
+        }
+    }
+
+    scores.sort_unstable();
+    let middle_score = scores[scores.len() / 2];
+
+    println!("Middle score: {}", middle_score);
+    middle_score
+}
+
+fn get_close_char_score(char: &char) -> usize {
+    match char {
+        '(' => 1,
+        '[' => 2,
+        '{' => 3,
+        '<' => 4,
+        _ => 0,
+    }
+}
+
+fn test_line(line: String) -> Result<Vec<char>, (char, char)> {
+    let mut open_chars = vec![];
     for char in line.chars() {
         let result = match char {
             '{' | '[' | '(' | '<' => {
-                stack.push(char);
+                open_chars.push(char);
                 None
             }
-            '}' => test_char(&mut stack, '{', char),
-            ']' => test_char(&mut stack, '[', char),
-            ')' => test_char(&mut stack, '(', char),
-            '>' => test_char(&mut stack, '<', char),
+            '}' => test_char(open_chars.pop(), '{', char),
+            ']' => test_char(open_chars.pop(), '[', char),
+            ')' => test_char(open_chars.pop(), '(', char),
+            '>' => test_char(open_chars.pop(), '<', char),
             _ => None,
         };
-        if let Some(result) = result {
-            return result;
-        }
+
+        match result {
+            Some(result) => return Err(result),
+            None => (),
+        };
     }
-    Ok(())
+    Ok(open_chars)
 }
 
 fn test_char(
-    stack: &mut Vec<char>,
-    expected_char: char,
-    actual_char: char,
-) -> Option<Result<(), (char, char)>> {
-    if let Some(expected) = stack.pop() {
-        if expected != expected_char {
-            return Some(Err((expected, actual_char)));
-        }
-    } else {
-        return Some(Err((expected_char, actual_char)));
+    open_char: Option<char>,
+    expected_open_char: char,
+    close_char: char,
+) -> Option<(char, char)> {
+    if open_char.is_none() || open_char.unwrap() != expected_open_char {
+        return Some((expected_open_char, close_char));
     }
+
     None
 }
